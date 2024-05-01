237923a96babac0b24303f2feee5f7597ba158ca
Braydon Bounous <bchadeb12@gmail.com>
2024-05-01 16:09:24 -0600

    Finished Unit 5

---
+++ server.py
@@ -0,0 +1,73 @@
+"""Server for movie ratings app."""
+
+from flask import (Flask, render_template, request, flash, session, redirect)
+from model import connect_to_db, db
+import crud
+
+from jinja2 import StrictUndefined
+
+app = Flask(__name__)
+app.secret_key = "dev"
+app.jinja_env.undefined = StrictUndefined
+
+@app.route('/')
+def homepage():
+    return render_template('homepage.html')
+
+@app.route("/movies")
+def all_movies():
+    movies = crud.get_movies()
+
+    return render_template("all_movies.html", movies=movies)
+
+@app.route("/movies/<movie_id>")
+def show_movie(movie_id):
+    """Show details on a particular movie."""
+
+    movie = crud.get_movie_by_id(movie_id)
+
+    return render_template("movie_details.html", movie=movie)
+
+@app.route("/users")
+def all_users():
+    """View all users."""
+
+    users = crud.get_users()
+
+    return render_template("all_users.html", users=users)
+
+@app.route("/users", methods=["POST"])
+def register_user():
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    user = crud.get_user_by_email(email)
+    if user:
+        flash("Invalid. Try again")
+    else:
+        user = crud.create_user(email, password)
+        db.session.add(user)
+        db.session.commit()
+        flash("Your account has been created!")
+
+    return redirect("/")
+
+@app.route("/login", methods=["POST"])
+def process_login():
+    """Process user login."""
+
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    user = crud.get_user_by_email(email)
+    if not user or user.password != password:
+        flash("The email or password you entered was incorrect.")
+    else:
+        session["user_email"] = user.email
+        flash(f"Welcome back, {user.email}!")
+
+    return redirect("/")
+
+if __name__ == "__main__":
+    connect_to_db(app)
+    app.run(host="0.0.0.0", debug=True)
